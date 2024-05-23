711f3aa1802843c393db55894eedb8c23b5aa75c
AbhayNT <abhay@nichetech.in>
2024-05-23 14:31:03 +0530

    Dynamic Bucket issue fixed

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require("express");
+const session = require("express-session");
 const fs = require("fs");
 const path = require("path");
 const AWS = require("aws-sdk");
-const app = express();
 const ejs = require("ejs");
 const multer = require("multer");
 const upload = multer();
-var bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 
 require("dotenv").config();
+
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -16,8 +18,16 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use("/icons", express.static("icons"));
 
-PORT = process.env.PORT || 4000;
-APPURL = process.env.APPURL || "127.0.0.1";
+// Set up session middleware
+app.use(session({
+  secret: 'your_secret_key', // replace with a secure key
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false } // Set to true if using HTTPS
+}));
+
+const PORT = process.env.PORT || 4000;
+const APPURL = process.env.APPURL || "127.0.0.1";
 
 app.listen(PORT, APPURL, () => {
   console.log(`app is running on port http://${APPURL}:${PORT}`);
@@ -25,13 +35,6 @@ app.listen(PORT, APPURL, () => {
 
 app.get("/", (req, res) => {
   res.render("login");
-});
-
-const spacesEndpoint = new AWS.Endpoint(process.env.SPACESENDPOINT);
-const s3 = new AWS.S3({
-  endpoint: spacesEndpoint,
-  accessKeyId: process.env.ACCESSKEYID,
-  secretAccessKey: process.env.SECRETACCESSKEY,
 });
 
 app.post("/login", (req, res) => {
@@ -50,19 +53,42 @@ app.post("/login", (req, res) => {
       console.error("Error:", err);
       res.status(500).send("Internal Server Error");
     } else {
-      process.env.ACCESSKEYID = accessKeyId;
-      process.env.SECRETACCESSKEY = secretAccessKey;
-      process.env.SPACESENDPOINT = spaceEndPoint;
-      process.env.BUCKET = bucketName;
+      req.session.accessKeyId = accessKeyId;
+      req.session.secretAccessKey = secretAccessKey;
+      req.session.spacesEndpoint = spaceEndPoint;
+      req.session.bucketName = bucketName;
 
       res.redirect("/folders");
     }
   });
 });
 
+function attachAWSConfig(req, res, next) {
+  const { accessKeyId, secretAccessKey, spacesEndpoint, bucketName } = req.session;
+
+  if (!accessKeyId || !secretAccessKey || !spacesEndpoint || !bucketName) {
+    return res.status(400).send("AWS credentials are not set. Please login.");
+  }
+
+  AWS.config.update({
+    accessKeyId: accessKeyId,
+    secretAccessKey: secretAccessKey,
+  });
+
+  req.s3 = new AWS.S3({ endpoint: new AWS.Endpoint(spacesEndpoint) });
+  req.bucketName = bucketName;
+
+  next();
+}
+
+app.use(attachAWSConfig);
+
 app.get("/files", (req, res) => {
+  const s3 = req.s3;
+  const bucketName = req.bucketName;
+
   const params = {
-    Bucket: process.env.BUCKET,
+    Bucket: bucketName,
     Prefix: req.query.folder || "",
   };
 
@@ -72,44 +98,30 @@ app.get("/files", (req, res) => {
       res.status(500).send("Internal Server Error");
     } else {
       const fileList = data.Contents.map(async (item) => {
-        const metaData = await s3
-          .headObject({ Bucket: process.env.BUCKET, Key: item.Key })
-          .promise();
+        const metaData = await s3.headObject({ Bucket: bucketName, Key: item.Key }).promise();
         return {
           Key: item.Key,
           LastModified: metaData.LastModified,
           Size: formatBytes(metaData.ContentLength),
           DownloadUrl: s3.getSignedUrl("getObject", {
-            Bucket: process.env.BUCKET,
+            Bucket: bucketName,
             Key: item.Key,
             Expires: 3600,
           }),
-          ImageUrl:
-            "http://" +
-            process.env.BUCKET +
-            "." +
-            process.env.SPACESENDPOINT +
-            "/" +
-            item.Key,
+          ImageUrl: "http://" + bucketName + "." + process.env.SPACESENDPOINT + "/" + item.Key,
         };
       });
       Promise.all(fileList)
         .then((files) => {
-          files.sort(
-            (a, b) => new Date(b.LastModified) - new Date(a.LastModified)
-          );
-          ejs.renderFile(
-            path.join(__dirname, "views", "files.ejs"),
-            { files: files },
-            (err, html) => {
-              if (err) {
-                console.error("Error rendering EJS file: ", err);
-                res.status(500).send("Internal Server Error");
-              } else {
-                res.send(html);
-              }
+          files.sort((a, b) => new Date(b.LastModified) - new Date(a.LastModified));
+          ejs.renderFile(path.join(__dirname, "views", "files.ejs"), { files: files }, (err, html) => {
+            if (err) {
+              console.error("Error rendering EJS file: ", err);
+              res.status(500).send("Internal Server Error");
+            } else {
+              res.send(html);
             }
-          );
+          });
         })
         .catch((error) => {
           console.error("Error getting file metadata: ", error);
@@ -128,16 +140,20 @@ function formatBytes(bytes) {
 }
 
 app.post("/upload", upload.single("file"), (req, res) => {
+  const s3 = req.s3;
+  const bucketName = req.bucketName;
   const file = req.file;
   const folderName = req.body.folderName;
+
   if (!file) {
     return res.status(400).send("No file uploaded");
   } else if (!folderName) {
-    return res.status(400).send("select foulder name");
+    return res.status(400).send("Select folder name");
   }
+
   const key = folderName + "/" + file.originalname;
   const params = {
-    Bucket: process.env.BUCKET,
+    Bucket: bucketName,
     Key: key,
     Body: file.buffer,
     ACL: 'public-read'
@@ -154,12 +170,16 @@ app.post("/upload", upload.single("file"), (req, res) => {
 });
 
 app.post("/remove", (req, res) => {
+  const s3 = req.s3;
+  const bucketName = req.bucketName;
   const key = req.body.key;
+
   if (!key) {
     return res.status(400).send("No file key provided");
   }
+
   const params = {
-    Bucket: process.env.BUCKET,
+    Bucket: bucketName,
     Key: key,
   };
 
@@ -174,8 +194,11 @@ app.post("/remove", (req, res) => {
 });
 
 app.get("/folders", (req, res) => {
+  const s3 = req.s3;
+  const bucketName = req.bucketName;
+
   const params = {
-    Bucket: process.env.BUCKET,
+    Bucket: bucketName,
     Delimiter: "/",
   };
 
